7fa0538796c53ca15ccca4c50c0fbe0e81928a6e
izzy <me@insrt.uk>
2026-04-01 12:52:57 +0100

    fix: setting configuration too early - causes crash

--- server/src/workers/api.ts
+++ server/src/workers/api.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { ModuleConfigRepository } from 'orchestration-api/dist';
 import { configureExpress, configureTelemetry } from 'src/app.common';
 import { ApiModule } from 'src/app.module';
 import { AppRepository } from 'src/repositories/app.repository';
 import { ApiService } from 'src/services/api.service';
-import { ServerService } from 'src/services/server.service';
-import { getExternalDomain, isStartUpError } from 'src/utils/misc';
+import { isStartUpError } from 'src/utils/misc';
 
 async function bootstrap() {
   process.title = 'immich-api';
@@ -15,11 +13,6 @@ async function bootstrap() {
 
   const app = await NestFactory.create<NestExpressApplication>(ApiModule, { bufferLogs: true });
   app.get(AppRepository).setCloseFn(() => app.close());
-
-  const config = await app.get(ServerService).getConfig({ withCache: true });
-  app.get(ModuleConfigRepository).update({
-    externalBaseUrl: getExternalDomain(config.server),
-  });
 
   void configureExpress(app, {
     ssr: ApiService,
